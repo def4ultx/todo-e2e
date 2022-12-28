f5a03b908d7f2002d22aefb120cc55852808517c
Sirapat Siribandit <sirapat@gideon-one.com>
2022-12-28 16:45:25 +0700

    add

--- calculator.go
+++ calculator.go
@@ -3,3 +3,7 @@ package main
 func Plus(a, b int) int {
 	return a + b
 }
+
+func Minus(a, b int) int {
+	return a + b
+}
--- calculator_test.go
+++ calculator_test.go
@@ -9,3 +9,11 @@ func TestPlus(t *testing.T) {
 		t.Errorf("got %v, want %v", actual, expected)
 	}
 }
+
+func TestMinus(t *testing.T) {
+	actual := Minus(1, 2)
+	expected := -1
+	if actual != expected {
+		t.Errorf("got %v, want %v", actual, expected)
+	}
+}
